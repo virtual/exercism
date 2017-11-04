ffcc7e056d72c92a414223fce5a5d42efc1450e1
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-11-03 19:17:05 -0600

    solved rna transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,9 +1,18 @@
-function DnaTranscriber(dna) {
-  this.dna = dna;
-}
+function DnaTranscriber() {}
 
-DnaTranscriber.prototype.toRna = function(){
-  return this.dna;
+DnaTranscriber.prototype.toRna = function(input){
+  if (/^[GCTA]+$/i.test(input)) {
+    const find = 'GCTA';
+    let updated = input.split("").map((e) => {
+      if (find.indexOf(e) !== -1) {  
+        return ['C', 'G', 'A', 'U'][find.indexOf(e)]
+      }
+    });
+    return updated.join("");
+  }
+  else {
+    throw new Error('Invalid input');
+  }
 }
 
 module.exports = DnaTranscriber;
